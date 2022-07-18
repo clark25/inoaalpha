143ec797a53a16e8c67aa59b959e6dc2196a14d5
clark25 <jonatas_s.v.s@hotmail.com>
2022-07-18 06:45:05 -0300

    primeiro passo sucess

--- inoa/alpha/views.py
+++ inoa/alpha/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from multiprocessing import context
+from django.views import generic
 
 # Create your views here.
+def index(request):
+  """View function for home page of site."""
+  return render(request, 'index.html')
