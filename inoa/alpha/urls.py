f9e0236d5a1ff954ebcd7829faafb30141df04cb
clark25 <jonatas_s.v.s@hotmail.com>
2022-07-19 21:04:26 -0300

    sobre criada

--- inoa/alpha/urls.py
+++ inoa/alpha/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('acoes/', views.AcaoListView.as_view(), name='acoes'),
   path('acao/<int:pk>', views.AcaoDetailView.as_view(), name='acao-detail'),
   path('acoesusuario/', views.AcaoUserView.as_view(), name='acao-user'),
+  path('sobre/', views.about, name='about'),
 ]
