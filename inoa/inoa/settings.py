c9b8f21943d28ff08b4127846a0d90ac4aec48be
clark25 <jonatas_s.v.s@hotmail.com>
2022-07-22 06:23:18 -0300

    apscheduler install

--- inoa/inoa/settings.py
+++ inoa/inoa/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Add our new application
-    'alpha.apps.AlphaConfig', 
+    'alpha.apps.AlphaConfig',
+    "django_apscheduler",
 ]
 
 MIDDLEWARE = [
